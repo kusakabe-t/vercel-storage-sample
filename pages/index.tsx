9bdb5f4a9c6b97a80184373c86812efafc0c1b46
kusakabe <kusakabe.takuya@alleeks.com>
2023-05-06 21:57:28 +0900

    feat kv の検証

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,22 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [kVId, setKVId] = useState(0)
+  const [kVEmail, setKVEmail] = useState('')
+
+  const getKVData = async () => {
+    const response = await fetch('/api/kv/user')
+    const { id, email } = await response.json()
+    setKVId(id)
+    setKVEmail(email)
+  }
   return (
+      <div>
+        <p>Vercel KV</p>
+        <button onClick={getKVData}>get kv data</button>
+        <div>
+          <p>id: {kVId}</p>
+          <p>email: {kVEmail}</p>
         </div>
       </div>
 
